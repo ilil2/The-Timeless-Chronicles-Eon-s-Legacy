319ac79fabc199f952cd0f63d1ac987bdb89f6c1
Corentin del Pozo <darkrentin@gamil.com>
2023-11-15 13:28:48 +0100

    Ajout de la Map du Level1

---
+++ Jeu/Client/Scripts/MapLvl1Script.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class MapLvl1Script : Node3D
+{
+	//public override void _Ready()
+	//{
+	//	MeshInstance3D plan = new MeshInstance3D();
+	//	plan.Mesh = new PlaneMesh();
+	//	AddChild(plan);
+	//
+	//}
+}
