ab4666d1fca7d6b2aa92ad4ec526ceb87f014ec9
Corentin del Pozo <darkrentin@gamil.com>
2023-11-22 13:50:09 +0100

    Ajout de l'algo de spawn de map

--- Jeu/Client/Scripts/MapLvl1Script.cs
+++ Jeu/Client/Scripts/MapLvl1Script.cs
@@ -1,81 +1,136 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
-public partial class MapLvl1Script : Node3D
+public partial class MapLvl1 : Node3D
 {
-	public static List<Node3D> RoomList;
-	private int _NbRoom = 1;
-	private int _LenMap = 18;
+	private int _NbRoom = 200;
+	private int _NbTypeRoom = 4;
+	private int _LenWall = 6;
 	private double _LenMin = 10;
+	private Dictionary<string,Node3D> Dico = new Dictionary<string,Node3D>();
 	public override void _Ready()
 	{
-		RoomList = CreateMap();
-		SupprDouble(RoomList);
+		for (int i = 1; i < _NbTypeRoom; i++)
+		{
+			PackedScene TG = GD.Load<PackedScene>($"res://Scenes/MapScenes/RoomScenes/Room{i}Grid.tscn");
+			Node3D Grid = TG.Instantiate<Node3D>();
+			Dico[$"res://Scenes/MapScenes/RoomScenes/Room{i}.tscn"] = Grid;
+		}
 		
+		Stopwatch stopwatch = new Stopwatch();
+		stopwatch.Start();
+		CreateMap();
+		stopwatch.Stop();
+		GD.Print($"Map créer en {stopwatch.Elapsed}");
 	}
 	
-
-	public List<Node3D> CreateMap()
+	public override void _Process(double delta)
 	{
-		//Fonction pour créer les salles et les ajouter a une liste
-		List<Node3D> RoomList = new List<Node3D>();
-		for (int i = -_NbRoom; i <= _NbRoom; i++)
+	}
+
+	public double Distance(Node3D Room1, Node3D Room2)
+	{
+		return Math.Sqrt(Math.Pow(Room1.Position.X - Room2.Position.X, 2) -
+						 Math.Pow(Room1.Position.X - Room2.Position.X, 2));
+	}
+
+	public bool CheckPlace(Node3D ActualRoom, List<Node3D> RoomList)
+	{
+		if (RoomList.Count == 0)
 		{
-			for (int j = -_NbRoom; j <= _NbRoom; j++)
-			{
-				PackedScene R = GD.Load<PackedScene>("res://Scenes/MapScenes/RoomScenes/Room1.tscn");
-				Node3D Room = R.Instantiate<Node3D>();
-				Room.Position = new Vector3(i * _LenMap, 0, j * _LenMap);
-				RoomList.Add(Room);
-			}
-
+			return true;
 		}
-		return RoomList;
-	}
-	
-	public void SupprDouble(List<Node3D> RoomList)
-	{
 		for (int i = 0; i < RoomList.Count; i++)
-		//Parcoure toutes les salles
 		{
-			Node3D ActualRoom = RoomList[i];
-			for (int j = i+1; j < RoomList.Count; j++)
-			//Parcoure toutes les autres salles
+			Node3D TestedRoom = RoomList[i];
+			double dist = Distance(ActualRoom, TestedRoom);
+			if (dist<=_LenMin)
 			{
-				Node3D TestedRoom = RoomList[j];
-				double Distance = Math.Sqrt((ActualRoom.Position.X+TestedRoom.Position.X)*(ActualRoom.Position.Y+TestedRoom.Position.Y)*(ActualRoom.Position.Z+TestedRoom.Position.Z));
-				//Verifie que il ne traite que les salles les plus proches 
-				if (Distance<_LenMin)
+				Node3D TestedGrid = Dico[TestedRoom.SceneFilePath];
+				Node3D ActualGrid = Dico[ActualRoom.SceneFilePath];
+				for (int j = 0; j < ActualGrid.GetChildCount(); j++)
 				{
-					for (int k = 0; k < ActualRoom.GetChildCount(); k++)
-					//Parcoure tout les murs de la salle
+					Node3D ActualTile = ActualGrid.GetChild<Node3D>(j);
+					//GD.Print($"Actual Tile = {j}");
+					for (int k = 0; k < TestedGrid.GetChildCount(); k++)
 					{
-						Node3D ActualChild = ActualRoom.GetChild<Node3D>(k);
-						for (int l = 0; l < TestedRoom.GetChildCount(); l++)
-						//Parcoure tout les murs d'une salle proche
+						Node3D TestedTile = TestedGrid.GetChild<Node3D>(k);
+						//GD.Print($"({ActualRoom.Position.X + ActualTile.Position.X},{ActualRoom.Position.Z + ActualTile.Position.Z}) = ({TestedRoom.Position.X + TestedTile.Position.X},{TestedRoom.Position.Z + TestedTile.Position.Z}) tile test {k} ");
+						bool pos = (((ActualRoom.Position.X + ActualTile.Position.X) == (TestedRoom.Position.X + TestedTile.Position.X)) &&
+							((ActualRoom.Position.Z + ActualTile.Position.Z) == (TestedRoom.Position.Z + TestedTile.Position.Z)));
+						if (pos)
 						{
-							Node3D TestedChild = TestedRoom.GetChild<Node3D>(l);
-							bool TestPos = (TestedChild.Position.X+TestedRoom.Position.X == ActualChild.Position.X+ActualRoom.Position.X)&&(TestedChild.Position.Y+TestedRoom.Position.Y == ActualChild.Position.Y+ActualRoom.Position.Y)&&(TestedChild.Position.Z+TestedRoom.Position.Z == ActualChild.Position.Z+ActualRoom.Position.Z);
-							//Test si il existe deux murs superposées
-							if (TestPos)
-							{
-								if (l<200)
-								{
-									//supprime le murs actuel
-									ActualChild.QueueFree();
-								}
-								//supprime le murs test
-								//TestedChild.QueueFree();
-								l = 200;
-							
-							}
+							return false;
 						}
-					}		
+					}
 				}
+
 			}
-			AddChild(ActualRoom);
 		}
+		return true;
 	}
 
+	public void CreateMap()
+	{
+		List<Node3D> RoomList = new List<Node3D>();
+
+		for (int i = 0; i < _NbRoom; i++)
+		{
+			int RandIntID = new Random().Next(1, 4);
+			int X = (int)new Random().Next(-21,22);
+			int Y = (int)new Random().Next(-21,22);
+		
+			PackedScene R = GD.Load<PackedScene>($"res://Scenes/MapScenes/RoomScenes/Room{RandIntID}.tscn");
+			Node3D Room = R.Instantiate<Node3D>();
+			Room.Position = new Vector3(X * _LenWall, 0, Y * _LenWall);
+
+			if (CheckPlace(Room,RoomList))
+				{	
+					for (int j = i+1; j < RoomList.Count; j++)
+					{
+						Node3D TestedRoom = RoomList[j];
+						double Dist = Distance(Room,TestedRoom);
+						if (Dist<_LenMin)
+						{
+							for (int k = 0; k < Room.GetChildCount(); k++)
+							{
+								Node3D ActualChild = Room.GetChild<Node3D>(k);
+								for (int l = 0; l < TestedRoom.GetChildCount(); l++)
+								{
+									Node3D TestedChild = TestedRoom.GetChild<Node3D>(l);
+									bool TestPos = (TestedChild.Position.X+TestedRoom.Position.X == ActualChild.Position.X+Room.Position.X)&&(TestedChild.Position.Z+TestedRoom.Position.Z == ActualChild.Position.Z+Room.Position.Z);
+									if (TestPos)
+									{
+										if (l<200)
+										{
+											ActualChild.QueueFree();
+											l = 200;
+											//if (RandInt==1)
+											//{
+											// 	Node3D Gate = AssetC.Instantiate<Node3D>();
+											//	Gate.Rotation = new Vector3(0,ActualChild.Rotation.Y,0);
+											//	Gate.Position = new Vector3(ActualChild.Position.X+ActualRoom.Position.X,ActualChild.Position.Y+ActualRoom.Position.Y,ActualChild.Position.Z+ActualRoom.Position.Z);
+											//	TestedChild.QueueFree();
+											//}
+										}
+									}
+								}
+							}
+				
+						}
+					}
+
+
+					RoomList.Add(Room);
+					AddChild(Room);
+				}
+		}
+
+		
+
+
+
+	}
 }
