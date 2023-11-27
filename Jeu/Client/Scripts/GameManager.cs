c205e9220090f9f238ad13e8df6a4ba3b60599df
narth <nathan.hirth538@gmail.com>
2023-11-27 13:51:10 +0100

    Maj 2 affichage autre joueur

--- Jeu/Client/Scripts/GameManager.cs
+++ Jeu/Client/Scripts/GameManager.cs
@@ -41,7 +41,7 @@ public partial class GameManager : Node3D
 	public override void _Ready()
 	{
 		soc = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);	//creation du socket
-		iep = new IPEndPoint(IPAddress.Parse("192.168.97.218"), 9191);						//adresse + port du serveur principal
+		iep = new IPEndPoint(IPAddress.Parse("192.168.1.218"), 9191);						//adresse + port du serveur principal
 		soc.Connect(iep);				//conexion
 		
 		
@@ -246,7 +246,7 @@ public partial class GameManager : Node3D
 				state = 4;
 				
 				soc2 = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);	//nouveau socket
-				iep2 = new IPEndPoint(IPAddress.Parse("192.168.97.218"), port_serv_jeu);				//nouvelle ip
+				iep2 = new IPEndPoint(IPAddress.Parse("192.168.1.218"), port_serv_jeu);				//nouvelle ip
 				soc2.Connect(iep2);																			//connexion
 			
 				ns2 = new NetworkStream(soc2);
