aeb9e70db7961e760d9342bc16b1ebb23d8350e2
narth <nathan.hirth538@gmail.com>
2023-12-01 17:25:11 +0100

    Patch multijoueur termine

--- Jeu/Client/Scripts/PlayerScripts/OtherAssassinScript.cs
+++ Jeu/Client/Scripts/PlayerScripts/OtherAssassinScript.cs
@@ -11,10 +11,28 @@ public partial class OtherAssassinScript : CharacterBody3D
 		EntityPosition = Position;
 	}
 
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
 		string[] Positions = GameManager.InfoAutreJoueur[$"co{ID}"].Split(";");
-		Position = new Vector3(Lib.Conversions.AtoF(Positions[0]), Lib.Conversions.AtoF(Positions[1]), Lib.Conversions.AtoF(Positions[2]));
+		Vector3 PositionA = new Vector3(Lib.Conversions.AtoF(Positions[0]), Lib.Conversions.AtoF(Positions[1]), Lib.Conversions.AtoF(Positions[2]));
+		Vector3 PositionD = Position;
+		Vector3 AddPosition = PositionA - PositionD;
+
+		if (Math.Sqrt(Lib.Conversions.Pow(AddPosition.X, 2) + Lib.Conversions.Pow(AddPosition.Y, 2) + Lib.Conversions.Pow(AddPosition.Z, 2)) >= 0.1f)
+		{
+			Vector3 Move = PositionA - PositionD;
+        
+			Vector3 velocity = Velocity;
+			velocity.Z = Move.Z;
+			velocity.X = Move.X;
+			velocity.Y = Move.Y;
+			Velocity = velocity;
+			MoveAndSlide();
+		}
+		else
+		{
+			Position = PositionA;
+		}
 	}
 	
 	public void SetID(int id)
