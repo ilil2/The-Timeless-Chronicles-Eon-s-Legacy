c9dc7c956b6dc51c44554cbc66784ac2b5fa18ca
Ilyann Gwinner <ilyann.gwinner@gmail.com>
2023-12-27 11:42:50 +0100

    Restore files

---
+++ restore.sh
@@ -0,0 +1,6 @@
+git restore --staged Jeu/Serveur/MainServeur/bin
+git restore --staged Jeu/Serveur/MainServeur/obj
+git restore --staged Jeu/Serveur/Serveur/bin
+git restore --staged Jeu/Serveur/Serveur/obj
+git restore --staged Jeu/Serveur/Lib/bin
+git restore --staged Jeu/Serveur/Lib/obj
