616fb58083fa93f95ec566a396216815c0e9d287
narth <nathan.hirth538@gmail.com>
2024-01-08 12:03:09 +0100

    Ajout de nouvelles langues

--- Jeu/Client/Scripts/Lib/LanguageControl.cs
+++ Jeu/Client/Scripts/Lib/LanguageControl.cs
@@ -194,6 +194,229 @@ public class LanguageControl
             {"settingsMenuControlsReload", "Recharger"},
             {"settingsMenuControlsChat", "Tchat"},
             {"settingsMenuControlsPause", "Pause"},
+        },
+        
+        new Dictionary<string, string>
+        {
+            {"languageName", "Duetsch"},
+            
+            //Connection Menu
+            {"connectionMenuTitle", "Verbindung"},
+            {"connectionMenuPseudoText", "Pseudo"},
+            {"connectionMenuPasswordText", "Passwort"},
+            {"connectionMenuConnectionButton", "Verbindung"},
+            {"connectionMenuNoAccountButton", "Kein Konto?"},
+            
+            //Inscription Menu
+            {"inscriptionMenuTitle", "Einschreibung"},
+            {"inscriptionMenuPseudoText", "Pseudo"},
+            {"inscriptionMenuPasswordText", "Passwort"},
+            {"inscriptionMenuPasswordConfirmText", "Passwort bestätigen"},
+            {"inscriptionMenuInscriptionButton", "Einschreibung"},
+            {"inscriptionMenuAlreadyAccountButton", "Bereits ein Konto?"},
+            
+            //Lobby Menu
+            {"lobbyMenuTitle", "Lobby"},
+            {"lobbyMenuCreateGame", "Spiel erstellen"},
+            {"lobbyMenuJoinGame", "Spiel beitreten"},
+            
+            //Create Game Menu
+            {"createGameMenuTitle", "Spiel erstellen"},
+            {"createGameMenuBackButton", "<= Zurück"},
+            {"createGameMenuStartGame", "Spiel starten"},
+            
+            //Join Game Menu
+            {"joinGameMenuTitle", "Spiel beitreten"},
+            {"joinGameMenuBackButton", "<= Zurück"},
+            {"joinGameMenuGameID", "Spiel-ID"},
+            
+            //Select Class Menu
+            {"selectClassMenuArcherClass", "Bogenschütze"},
+            {"selectClassMenuKnightClass", "Ritter"},
+            {"selectClassMenuScientistClass", "Wissenschaftler"},
+            {"selectClassMenuAssassinClass", "Attentäter"},
+            {"selectClassMenuReadyButton", "Bereit"},
+            {"selectClassMenuWaitingText", "Warten auf die anderen Spieler..."},
+            
+            //Pause Menu
+            {"pauseMenuTitle", "Pause Menü"},
+            {"pauseMenuResumeButton", "Fortsetzen"},
+            {"pauseMenuSettingsButton", "Einstellungen"},
+            {"pauseMenuLeaveButton", "Verlassen"},
+            {"pauseMenuConfirmLeaveTitle", "Bist du sicher, dass du das Spiel verlassen willst?"},
+            {"pauseMenuConfirmLeaveButton", "Verlassen"},
+            {"pauseMenuCancelLeaveButton", "Abbrechen"},
+            
+            //Settings Menu
+            {"settingsMenuTitle", "Einstellungen"},
+            {"settingsMenuBackButton", "<= Zurück"},
+            {"settingsMenuResetButton", "Zurücksetzen"},
+            {"settingsMenuSaveButton", "Speichern"},
+            
+            //Settings Menu - Game
+            {"settingsMenuGameButton", "Spiel"},
+            {"settingsMenuGameLanguage", "Sprache :"},
+            {"settingsMenuGameMouseSensibility", "Mausempfindlichkeit :"},
+            {"settingsMenuGameFullScreen", "Vollbild :"},
+            {"settingsMenuGameEnableChat", "Chat aktivieren :"},
+            {"settingsMenuGameChatSize", "Chatgröße :"},
+            {"settingsMenuGameChatSizeSmall", "Klein"},
+            {"settingsMenuGameChatSizeMedium", "Mittel"},
+            {"settingsMenuGameChatSizeLarge", "Groß"},
+            
+            //Settings Menu - Audio
+            {"settingsMenuAudioButton", "Audio"},
+            
+            //Settings Menu - Video
+            {"settingsMenuVideoButton", "Video"},
+            
+            //Settings Menu - Controls
+            {"settingsMenuControlsButton", "Steuerung"},
+            {"settingsMenuControlsMoveForward", "Vorwärts"},
+            {"settingsMenuControlsMoveBackward", "Rückwärts"},
+            {"settingsMenuControlsMoveLeft", "Links"},
+            {"settingsMenuControlsMoveRight", "Rechts"},
+            {"settingsMenuControlsSprint", "Sprinten"},
+            {"settingsMenuControlsDash", "Dash"},
+            {"settingsMenuControlsCapacity1", "Fähigkeit 1"},
+            {"settingsMenuControlsCapacity2", "Fähigkeit 2"},
+            {"settingsMenuControlsCapacity3", "Fähigkeit 3"},
+            {"settingsMenuControlsItem1", "Gegenstand 1"},
+            {"settingsMenuControlsItem2", "Gegenstand 2"},
+            {"settingsMenuControlsItem3", "Gegenstand 3"},
+            {"settingsMenuControlsInventory", "Inventar"},
+            {"settingsMenuControlsReload", "Nachladen"},
+            {"settingsMenuControlsChat", "Chat"},
+            {"settingsMenuControlsPause", "Pause"},
+        },
+        
+        new Dictionary<string, string>
+        {
+            {"languageName", "Español"},
+            
+            //Connection Menu
+            {"connectionMenuTitle", "Conexión"},
+            {"connectionMenuPseudoText", "Pseudo"},
+            {"connectionMenuPasswordText", "Contraseña"},
+            {"connectionMenuConnectionButton", "Conexión"},
+            {"connectionMenuNoAccountButton", "¿No tienes una cuenta?"},
+            
+            //Inscription Menu
+            {"inscriptionMenuTitle", "Inscripción"},
+            {"inscriptionMenuPseudoText", "Pseudo"},
+            {"inscriptionMenuPasswordText", "Contraseña"},
+            {"inscriptionMenuPasswordConfirmText", "Confirmar contraseña"},
+            {"inscriptionMenuInscriptionButton", "Inscripción"},
+            {"inscriptionMenuAlreadyAccountButton", "¿Ya tienes una cuenta?"},
+            
+            //Lobby Menu
+            {"lobbyMenuTitle", "Lobby"},
+            {"lobbyMenuCreateGame", "Crear juego"},
+            {"lobbyMenuJoinGame", "Unirse al juego"},
+            
+            //Create Game Menu
+            {"createGameMenuTitle", "Crear juego"},
+            {"createGameMenuBackButton", "<= Regreso"},
+            {"createGameMenuStartGame", "Comenzar el juego"},
+            
+            //Join Game Menu
+            {"joinGameMenuTitle", "Unirse al juego"},
+            {"joinGameMenuBackButton", "<= Regreso"},
+            {"joinGameMenuGameID", "ID del juego"},
+            
+            //Select Class Menu
+            {"selectClassMenuArcherClass", "Arquero"},
+            {"selectClassMenuKnightClass", "Caballero"},
+            {"selectClassMenuScientistClass", "Científico"},
+            {"selectClassMenuAssassinClass", "Asesino"},
+            {"selectClassMenuReadyButton", "Listo"},
+            {"selectClassMenuWaitingText", "Esperando a los otros jugadores..."},
+            
+            //Pause Menu
+            {"pauseMenuTitle", "Menú de pausa"},
+            {"pauseMenuResumeButton", "Reanudar"},
+            {"pauseMenuSettingsButton", "Ajustes"},
+            {"pauseMenuLeaveButton", "Dejar"},
+            {"pauseMenuConfirmLeaveTitle", "¿Estás seguro de que quieres dejar el juego?"},
+            {"pauseMenuConfirmLeaveButton", "Dejar"},
+            {"pauseMenuCancelLeaveButton", "Cancelar"},
+            
+            //Settings Menu
+            {"settingsMenuTitle", "Ajustes"},
+            {"settingsMenuBackButton", "<= Regreso"},
+            {"settingsMenuResetButton", "Reiniciar"},
+            {"settingsMenuSaveButton", "Guardar"},
+            
+            //Settings Menu - Game
+            {"settingsMenuGameButton", "Juego"},
+            {"settingsMenuGameLanguage", "Idioma :"},
+            {"settingsMenuGameMouseSensibility", "Sensibilidad del ratón :"},
+            {"settingsMenuGameFullScreen", "Pantalla completa :"},
+            {"settingsMenuGameEnableChat", "Activar el chat :"},
+            {"settingsMenuGameChatSize", "Tamaño del chat :"},
+            {"settingsMenuGameChatSizeSmall", "Pequeño"},
+            {"settingsMenuGameChatSizeMedium", "Medio"},
+            {"settingsMenuGameChatSizeLarge", "Grande"},
+            
+            //Settings Menu - Audio
+            {"settingsMenuAudioButton", "Audio"},
+            
+            //Settings Menu - Video
+            {"settingsMenuVideoButton", "Video"},
+            
+            //Settings Menu - Controls
+            {"settingsMenuControlsButton", "Controles"},
+            {"settingsMenuControlsMoveForward", "Adelante"},
+            {"settingsMenuControlsMoveBackward", "Atrás"},
+            {"settingsMenuControlsMoveLeft", "Izquierda"},
+            {"settingsMenuControlsMoveRight", "Derecha"},
+            {"settingsMenuControlsSprint", "Sprint"},
+            {"settingsMenuControlsDash", "Dash"},
+            {"settingsMenuControlsCapacity1", "Capacidad 1"},
+            {"settingsMenuControlsCapacity2", "Capacidad 2"},
+            {"settingsMenuControlsCapacity3", "Capacidad 3"},
+            {"settingsMenuControlsItem1", "Objeto 1"},
+            {"settingsMenuControlsItem2", "Objeto 2"},
+            {"settingsMenuControlsItem3", "Objeto 3"},
+            {"settingsMenuControlsInventory", "Inventario"},
+            {"settingsMenuControlsReload", "Recargar"},
+            {"settingsMenuControlsChat", "Chat"},
+            {"settingsMenuControlsPause", "Pausa"},
+        },
+        
+        new Dictionary<string, string>
+        {
+            {"languageName", "Ελληνική"},
+            
+            //Connection Menu
+            {"connectionMenuTitle", "Σύνδεση"},
+            {"connectionMenuPseudoText", "Pseudo"},
+            {"connectionMenuPasswordText", "Κωδικός πρόσβασης"},
+            {"connectionMenuConnectionButton", "Σύνδεση"},
+            {"connectionMenuNoAccountButton", "Δεν έχετε λογαριασμό;"},
+            
+            //Inscription Menu
+            {"inscriptionMenuTitle", "Εγγραφή"},
+            {"inscriptionMenuPseudoText", "Pseudo"},
+            {"inscriptionMenuPasswordText", "Κωδικός πρόσβασης"},
+            {"inscriptionMenuPasswordConfirmText", "Επιβεβαίωση κωδικού πρόσβασης"},
+            {"inscriptionMenuInscriptionButton", "Εγγραφή"},
+            {"inscriptionMenuAlreadyAccountButton", "Έχετε ήδη ένα λογαριασμό;"},
+            
+            //Lobby Menu
+            {"lobbyMenuTitle", "Σαλόνι"},
+            {"lobbyMenuCreateGame", "Δημιουργία παιχνιδιού"},
+            {"lobbyMenuJoinGame", "Ενταχθείτε στο παιχνίδι"},
+            
+            //Create Game Menu
+            {"createGameMenuTitle", "Δημιουργία παιχνιδιού"},
+            {"createGameMenuBackButton", "<= Πίσω"},
+            {"createGameMenuStartGame", "Ξεκινήστε το παιχνίδι"},
+            
+            //Join Game Menu
+            {"joinGameMenuTitle", "Ενταχθείτε στο παιχνίδι"},
+            {"joinGameMenuBackButton", "<= Πίσω"},
+            {"joinGameMenuGameID", "Αναγνωριστικό παιχνιδιού"},
         }
     };
     
