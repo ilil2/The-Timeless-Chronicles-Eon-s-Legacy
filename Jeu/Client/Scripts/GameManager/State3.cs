625a1732f6db9279ee2f7d77a3735847edd17765
narth <nathan.hirth538@gmail.com>
2024-01-12 17:46:42 +0100

    Patch connection serveur

--- Jeu/Client/Scripts/GameManager/State3.cs
+++ Jeu/Client/Scripts/GameManager/State3.cs
@@ -10,53 +10,31 @@ public partial class State3 : GameManager
 {
     public static void State()
     {
-        if (ScanPorts(IP,port_serv_jeu))
+        if (OnJoin)
         {
-            if (OnJoin)
-            {
-                th.Interrupt();				//fermeture du thread listen
-                thread = false;
-            }
-            ns.Close();
-            tw.Close();					//fermeture envoi requette au serveur principal
-            tr.Close();					//fermeture recu requette du serveur principal
-            soc.Disconnect(false);		//deconnection du socket
-            state = 4;
-
-            soc2 = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);	//nouveau socket
-            soc2.ReceiveTimeout = 60000;
-            iep2 = new IPEndPoint(IPAddress.Parse(IP), port_serv_jeu);				//nouvelle ip
-            soc2.Connect(iep2);																			//connexion
-			
-            ns2 = new NetworkStream(soc2);
-            tw2 = new StreamWriter(ns2);					//lecture serveur secondaire
-            tr2 = new StreamReader(ns2);					//ecriture serveur secondaire
-				
-            tw2.WriteLine(InfoJoueur["pseudo"]);
-            tw2.Flush();
-
-            th2 = new Thread(Listen2);	//initialisation thread
-            th2.Start();						//debut du thread
+            th.Interrupt();                //fermeture du thread listen
+            thread = false;
         }
-    }
+        ns.Close();
+        tw.Close();                    //fermeture envoi requette au serveur principal
+        tr.Close();                    //fermeture recu requette du serveur principal
+        soc.Disconnect(false);        //deconnection du socket
 
-    public static bool ScanPorts(string ipAddress, int port)
-    {
-        bool b = false;
-        TcpListener tcpListener = null;
+        soc2 = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);    //nouveau socket
+        soc2.ReceiveTimeout = 60000;
+        iep2 = new IPEndPoint(IPAddress.Parse(IP), port_serv_jeu);                //nouvelle ip
+        soc2.Connect(iep2);                                                                            //connexion
+            
+        ns2 = new NetworkStream(soc2);
+        tw2 = new StreamWriter(ns2);                    //lecture serveur secondaire
+        tr2 = new StreamReader(ns2);                    //ecriture serveur secondaire
+                
+        tw2.WriteLine(InfoJoueur["pseudo"]);
+        tw2.Flush();
 
-        try
-        {
-            tcpListener = new TcpListener(IPAddress.Parse(ipAddress), port);
-            tcpListener.Start();
-            b = true;
-        }
-        catch
-        {
-            b = false;
-        }
+        th2 = new Thread(Listen2);    //initialisation thread
+        th2.Start();                        //debut du thread
+        state = 4;
         
-        tcpListener?.Stop();
-        return b;
     }
 }
