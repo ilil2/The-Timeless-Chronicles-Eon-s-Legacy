f83d0b285affabd8babf65b6aad6a09f5d4fc212
Corentin del Pozo <darkrentin@gmail.com>
2024-01-25 13:53:14 +0100

    algo Maplvl2 T1

--- Jeu/Client/Scripts/GameManager/State6.cs
+++ Jeu/Client/Scripts/GameManager/State6.cs
@@ -3,117 +3,117 @@ using Lib;
 
 public partial class State6 : GameManager
 {
-    public static void State(double delta)
-    {
-        if (!Quit)
-            UDP.Send(soc2,InfoJoueur["id"] + "_" + "in:co:" + InfoJoueur["co"] + "/" + InfoJoueur["orientation"],iep2);
-        ((MapLvl1Script)Map).DebugMode(delta, Joueur1);
+	public static void State(double delta)
+	{
+		if (!Quit)
+			UDP.Send(soc2,InfoJoueur["id"] + "_" + "in:co:" + InfoJoueur["co"] + "/" + InfoJoueur["orientation"],iep2);
+		((MapLvl1Script)Map).DebugMode(delta, Joueur1);
 
-        if (!_pausemode && Lib.Conversions.ItoB(SettingsManager.GetAllSettings()["enableChat"]))
-        {
-            Chat();
-        }
-        else
-        {
-            _chat.Visible = false;
-        }
-        
-        if (InfoJoueur["attack"] != "")
-        {
-            UDP.Send(soc2,InfoJoueur["id"] + "_" + "on:" + InfoJoueur["attack"],iep2);
-            InfoJoueur["attack"] = "";
-        }
-    }
+		if (!_pausemode && Lib.Conversions.ItoB(SettingsManager.GetAllSettings()["enableChat"]))
+		{
+			Chat();
+		}
+		else
+		{
+			_chat.Visible = false;
+		}
+		
+		if (InfoJoueur["attack"] != "")
+		{
+			UDP.Send(soc2,InfoJoueur["id"] + "_" + "on:" + InfoJoueur["attack"],iep2);
+			InfoJoueur["attack"] = "";
+		}
+	}
 
-    private static void Chat()
-    {
-        _chat.Visible = true;
-        string commandchat = ((ChatUI)_chat).Inputtext;
-        if (commandchat != "")
-        {
-            if (commandchat[0] == '/')
-            {
-                commandchat = commandchat.Substring(1);
+	private static void Chat()
+	{
+		_chat.Visible = true;
+		string commandchat = ((ChatUI)_chat).Inputtext;
+		if (commandchat != "")
+		{
+			if (commandchat[0] == '/')
+			{
+				commandchat = commandchat.Substring(1);
 
-                if (Cheat)
-                {
-                    if (commandchat == "debug")
-                    {
-                        DebugMode = !DebugMode;
-                    }
-                    else if (commandchat == "display")
-                    {
-                        DebugMode = DebugMode;
-                    }
-                    else if (commandchat.Length > 3 && commandchat.Substring(0,4) == "help")
-                    {
-                        if (commandchat.Length == 4)
-                        {
-                            ((ChatUI)_chat).Outputaddtext = "Effectuez \"/help <numero de page>\" pour charger une page ou \"/help <commande>\" pour de l'aide sur une commande précise";
-                        }
-                        else
-                        {
-                            commandchat = commandchat.Substring(5);
-                            if (commandchat == "1")
-                            {
-                                ((ChatUI)_chat).Outputaddtext = "Page 1 : \n\tdebug - passe en camera debug\n\tdisplay - affiche les infos utiles\n\tsetrule - change une règle du jeu";
-                            }
-                            else if (commandchat == "setrule")
-                            {
-                                ((ChatUI)_chat).Outputaddtext = "setrule : \n\tfog <on/off> - active ou désactive le fog";
-                            }
-                            else
-                            {
-                                ((ChatUI)_chat).Outputaddtext = "Effectuez \"/help <numero de page>\" pour charger une page ou \"/help <commande>\" pour de l'aide sur une commande précise";
-                            }
-                        }
-                    }
-                    else if (commandchat.Length >= 8 && commandchat.Substring(0, 7) == "setrule")
-                    {
-                        commandchat = commandchat.Substring(8);
-                        GD.Print(commandchat);
-                        if (commandchat.Length >= 4 && commandchat.Substring(0,3) == "fog")
-                        {
-                            commandchat = commandchat.Substring(4);
-                            if (commandchat == "off")
-                            {
-                                Fog = false;
-                            }
-                            else if (commandchat == "on")
-                            {
-                                Fog = true;
-                            }
-                        }
-                        if (commandchat.Substring(0,2) == "ia")
-                        {
-                            commandchat = commandchat.Substring(4);
-                            if (commandchat.Substring(0,3) == "off")
-                            {
-                                DebugMode = DebugMode;
-                            }
-                            else if (commandchat.Substring(0,2) == "on")
-                            {
-                                DebugMode = DebugMode;
-                            }
-                        }
-                    }
-                }
-                else if (commandchat == "cheat on")
-                {
-                    Cheat = true;
-                    ((ChatUI)_chat).Outputaddtext = "passage en mode triche";
+				if (Cheat)
+				{
+					if (commandchat == "debug")
+					{
+						DebugMode = !DebugMode;
+					}
+					else if (commandchat == "display")
+					{
+						DebugMode = DebugMode;
+					}
+					else if (commandchat.Length > 3 && commandchat.Substring(0,4) == "help")
+					{
+						if (commandchat.Length == 4)
+						{
+							((ChatUI)_chat).Outputaddtext = "Effectuez \"/help <numero de page>\" pour charger une page ou \"/help <commande>\" pour de l'aide sur une commande précise";
+						}
+						else
+						{
+							commandchat = commandchat.Substring(5);
+							if (commandchat == "1")
+							{
+								((ChatUI)_chat).Outputaddtext = "Page 1 : \n\tdebug - passe en camera debug\n\tdisplay - affiche les infos utiles\n\tsetrule - change une règle du jeu";
+							}
+							else if (commandchat == "setrule")
+							{
+								((ChatUI)_chat).Outputaddtext = "setrule : \n\tfog <on/off> - active ou désactive le fog";
+							}
+							else
+							{
+								((ChatUI)_chat).Outputaddtext = "Effectuez \"/help <numero de page>\" pour charger une page ou \"/help <commande>\" pour de l'aide sur une commande précise";
+							}
+						}
+					}
+					else if (commandchat.Length >= 8 && commandchat.Substring(0, 7) == "setrule")
+					{
+						commandchat = commandchat.Substring(8);
+						GD.Print(commandchat);
+						if (commandchat.Length >= 4 && commandchat.Substring(0,3) == "fog")
+						{
+							commandchat = commandchat.Substring(4);
+							if (commandchat == "off")
+							{
+								Fog = false;
+							}
+							else if (commandchat == "on")
+							{
+								Fog = true;
+							}
+						}
+						if (commandchat.Substring(0,2) == "ia")
+						{
+							commandchat = commandchat.Substring(4);
+							if (commandchat.Substring(0,3) == "off")
+							{
+								DebugMode = DebugMode;
+							}
+							else if (commandchat.Substring(0,2) == "on")
+							{
+								DebugMode = DebugMode;
+							}
+						}
+					}
+				}
+				else if (commandchat == "cheat on")
+				{
+					Cheat = true;
+					((ChatUI)_chat).Outputaddtext = "passage en mode triche";
 
-                }
-                else if (commandchat == "help")
-                {
-                    ((ChatUI)_chat).Outputaddtext = "effectuez \"/cheat on\" pour passer en mode triche";
-                }
-            }
-            else
-            {
-                UDP.Send(soc2,InfoJoueur["id"] + "_" + "chat:" + ((ChatUI)_chat).Inputtext,iep2);
-            }
-            ((ChatUI)_chat).Inputtext = "";
-        }
-    }
+				}
+				else if (commandchat == "help")
+				{
+					((ChatUI)_chat).Outputaddtext = "effectuez \"/cheat on\" pour passer en mode triche";
+				}
+			}
+			else
+			{
+				UDP.Send(soc2,InfoJoueur["id"] + "_" + "chat:" + ((ChatUI)_chat).Inputtext,iep2);
+			}
+			((ChatUI)_chat).Inputtext = "";
+		}
+	}
 }
--- Jeu/Client/Scripts/MapScript/MapLvl2Script.cs
+++ Jeu/Client/Scripts/MapScript/MapLvl2Script.cs
@@ -1,27 +1,54 @@
 using Godot;
 using System;
 using System.Collections.Generic;
-[Tool]
+
 public partial class MapLvl2Script : Node3D, IMap
 {
-	[Export] private int Rayon = 0;
-	[Export] private int Pas = 360;
+
 	private Random Rand = new Random();
 	private PackedScene Wa = GD.Load<PackedScene>("res://Scenes/MapScenes/Lvl2/R.tscn");
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		for (int i = 0; i<360; i+=Pas)
+		 CreateBorder();
+
+		const int nbtree = 500;
+		const int radius = 1;
+		for (int i = 0; i < nbtree; i++)
 		{
-			float rot = Mathf.DegToRad(i);
-			Node3D W = Wa.Instantiate<Node3D>();
-			Node3D Wall = W.GetNode<Node3D>($"R{Rand.Next(1,5)}");
-			W.RemoveChild(Wall);
-			double X = Math.Cos(rot);
-			double Z = Math.Sin(rot);
-			Wall.Position = new Vector3((float)X*Rayon,0,(float)Z*Rayon);
-			Wall.Rotation += new Vector3(0,-rot,0);
-			AddChild(Wall);
+			RigidBody3D Sphere = new RigidBody3D();
+			SphereShape3D sphereShape = new SphereShape3D();
+			SphereMesh sphereMesh = new SphereMesh();
+			CollisionShape3D collisionShape = new CollisionShape3D();
+			MeshInstance3D meshInstance = new MeshInstance3D();
+			sphereMesh.Radius = radius;
+			sphereMesh.Height = radius * 2;
+			meshInstance.Mesh = sphereMesh;
+			sphereShape.Radius = radius;
+			collisionShape.Shape = sphereShape;
+			Sphere.AddChild(meshInstance);
+			Sphere.AddChild(collisionShape);
+
+			const int radiusmap = 150;
+			double t = 2 * Math.PI * Rand.NextDouble();
+			double u = Rand.NextDouble() + Rand.NextDouble();
+			double? r = null;
+			if (u>1)
+			{
+				r = 2 - u;
+			}
+			else
+			{
+				r = u;
+			}
+
+			double? x = radiusmap * r * Math.Cos(t);
+			double? z = radiusmap * r * Math.Sin(t);
+
+			Sphere.Position = new Vector3((float)x, 20, (float)z);
+			AddChild(Sphere);
+
+
 		}
 		
 	}
@@ -44,5 +71,23 @@ public partial class MapLvl2Script : Node3D, IMap
 	public void DebugMode(double delta, CharacterBody3D Player)
 	{
 		throw new NotImplementedException();
+	}
+	
+	private void CreateBorder()
+	{
+		const int Rayon = 150;
+		const int Pas = 4;
+		for (int i = 0; i<360; i+=Pas)
+		{
+			float rot = Mathf.DegToRad(i);
+			Node3D W = Wa.Instantiate<Node3D>();
+			Node3D Wall = W.GetNode<Node3D>($"R{Rand.Next(1,5)}");
+			W.RemoveChild(Wall);
+			double X = Math.Cos(rot);
+			double Z = Math.Sin(rot);
+			Wall.Position = new Vector3((float)X*Rayon,0,(float)Z*Rayon);
+			Wall.Rotation += new Vector3(0,-rot,0);
+			AddChild(Wall);
+		}
 	}
 }
