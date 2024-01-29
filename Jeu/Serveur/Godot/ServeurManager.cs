7596b523c4ce7ab7c81666a42dc487510d945d0a
Ilyann Gwinner <ilyann.gwinner@gmail.com>
2024-01-29 11:58:22 +0100

    ajout du EXEC chaque jour

--- Jeu/Serveur/Godot/ServeurManager.cs
+++ Jeu/Serveur/Godot/ServeurManager.cs
@@ -2,9 +2,17 @@ using Godot;
 using System;
 using System.IO;
 using System.Diagnostics;
+using Microsoft;
 
 public partial class ServeurManager : Node3D
 {
+	private int DateDay = DateTime.Now.Day;
+	private int DateDay2 = DateTime.Now.Day;
+	
+	private int Seed = 42;
+	private int AleateSeed = 42;
+	
+	private Random rand = new Random();
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -20,5 +28,18 @@ public partial class ServeurManager : Node3D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		DateDay2 = DateTime.Now.Day;
+		if (DateDay2 != DateDay)
+		{
+			Exec();
+			DateDay = DateDay2;
+		}
+	}
+
+	//a effectuer tout les jours
+	private void Exec()
+	{
+		Seed = rand.Next(0, 1000000);
+		AleateSeed = rand.Next(0, 1000000);
 	}
 }
