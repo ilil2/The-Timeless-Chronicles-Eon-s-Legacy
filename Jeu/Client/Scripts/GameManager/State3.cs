b7dccaea3bab3e561be045da6a0d15371723bf7c
Ilyann Gwinner <ilyann.gwinner@gmail.com>
2024-02-02 08:40:56 +0100

    prrémerge

--- Jeu/Client/Scripts/GameManager/State3.cs
+++ Jeu/Client/Scripts/GameManager/State3.cs
@@ -5,32 +5,32 @@ using Lib;
 
 public partial class State3 : GameManager
 {
-	public static void State()
-	{
-		if (OnJoin)
-		{
-			th.Interrupt();                //fermeture du thread listen
-			thread = false;
-		}
-		ns.Close();
-		tw.Close();                    //fermeture envoi requette au serveur principal
-		tr.Close();                    //fermeture recu requette du serveur principal
-		soc.Disconnect(false);        //deconnection du socket
+    public static void State()
+    {
+        if (OnJoin)
+        {
+            th.Interrupt();                //fermeture du thread listen
+            thread = false;
+        }
+        ns.Close();
+        tw.Close();                    //fermeture envoi requette au serveur principal
+        tr.Close();                    //fermeture recu requette du serveur principal
+        soc.Disconnect(false);        //deconnection du socket
 
-		soc2 = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);    //nouveau socket
-		soc2.ReceiveTimeout = 60000;
-		iep2 = new IPEndPoint(IPAddress.Parse(IP), port_serv_jeu);                //nouvelle ip
-		
-		UDP.Send(soc2,"connect",iep2);    //envoie requette de connection au serveur secondaire
-		InfoJoueur["id"] = UDP.Receive(soc2);    //reception de l'ID du serveur secondaire
-		((IMap)Map).SetSeed(Conversions.AtoI(InfoJoueur["id"].Split('*')[0]),Conversions.AtoI(InfoJoueur["id"].Split('*')[1]));
-		InfoJoueur["id"] = InfoJoueur["id"].Substring(0,1);
-		UDP.Send(soc2,$"{InfoJoueur["id"]}/{InfoJoueur["pseudo"]}",iep2);    //envoie du pseudo au serveur secondaire
-		
+        soc2 = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);    //nouveau socket
+        soc2.ReceiveTimeout = 60000;
+        iep2 = new IPEndPoint(IPAddress.Parse(IP), port_serv_jeu);                //nouvelle ip
+        
+        UDP.Send(soc2,"connect",iep2);    //envoie requette de connection au serveur secondaire
+        InfoJoueur["id"] = UDP.Receive(soc2);    //reception de l'ID du serveur secondaire
+        ((IMap)Map).SetSeed(Conversions.AtoI(InfoJoueur["id"].Split('*')[0]),Conversions.AtoI(InfoJoueur["id"].Split('*')[1]));
+        InfoJoueur["id"] = InfoJoueur["id"].Substring(0,1);
+        UDP.Send(soc2,$"{InfoJoueur["id"]}/{InfoJoueur["pseudo"]}",iep2);    //envoie du pseudo au serveur secondaire
+        
 
-		th2 = new Thread(Listen2);    //initialisation thread
-		th2.Start();                        //debut du thread
-		state = 4;
-		
-	}
+        th2 = new Thread(Listen2);    //initialisation thread
+        th2.Start();                        //debut du thread
+        state = 4;
+        
+    }
 }
--- Jeu/Client/Scripts/GameManager/State4.cs
+++ Jeu/Client/Scripts/GameManager/State4.cs
@@ -2,31 +2,31 @@ using Lib;
 
 public partial class State4 : GameManager
 {
-	public int LoadBar = 0;
-	public static void State()
-	{
-		if (MapOnLoad)
-		{
-			((ProgressBarMapLvl1)ProgressBar).Load = ((IMap)Map).Step();
-		}
-		if (ClassSelectUI.ClassChose != "")
-		{
-			InfoJoueur["class"] = ClassSelectUI.ClassChose;
-			ClassSelectUI.ClassChose = "";
-			UDP.Send(soc2,$"{InfoJoueur["id"]}/{InfoJoueur["class"]}",iep2);
-		}
-		else if (((IMap)Map).MapIsReady() && !StartMap)
-		{
-			Map.Visible = false;
-			UDP.Send(soc2,"load",iep2);
-			MapOnLoad = false;
-			((ProgressBarMapLvl1)ProgressBar).Load = 2500;
-		}
-		else if (StartMap)
-		{
-			ProgressBar.QueueFree();
-			Map.Visible = true;
-			state = 5;
-		}
-	}
+    public int LoadBar = 0;
+    public static void State()
+    {
+        if (MapOnLoad)
+        {
+            ((ProgressBarMapLvl1)ProgressBar).Load = ((MapLvl1Script)Map).step;
+        }
+        if (ClassSelectUI.ClassChose != "")
+        {
+            InfoJoueur["class"] = ClassSelectUI.ClassChose;
+            ClassSelectUI.ClassChose = "";
+            UDP.Send(soc2,$"{InfoJoueur["id"]}/{InfoJoueur["class"]}",iep2);
+        }
+        else if (((MapLvl1Script)Map).MapIsReady() && !StartMap)
+        {
+            Map.Visible = false;
+            UDP.Send(soc2,"load",iep2);
+            MapOnLoad = false;
+            ((ProgressBarMapLvl1)ProgressBar).Load = 2500;
+        }
+        else if (StartMap)
+        {
+            ProgressBar.QueueFree();
+            Map.Visible = true;
+            state = 5;
+        }
+    }
 }
