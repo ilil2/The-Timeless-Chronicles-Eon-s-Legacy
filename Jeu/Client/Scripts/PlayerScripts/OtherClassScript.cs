3f33aa4f1a14c3da85d8dc06f1f8e97c779dc10e
Corentin del Pozo <darkrentin@gmail.com>
2024-02-09 12:37:40 +0100

    Patch OtherScript

--- Jeu/Client/Scripts/PlayerScripts/OtherClassScript.cs
+++ Jeu/Client/Scripts/PlayerScripts/OtherClassScript.cs
@@ -27,8 +27,12 @@ public partial class OtherClassScript : CharacterBody3D
 
 	protected void SetPosition()
 	{
-		string[] positions = GameManager.InfoAutreJoueur[$"co{Id}"].Split(";");
-		Position = new Vector3(Lib.Conversions.AtoF(positions[0]), Lib.Conversions.AtoF(positions[1]), Lib.Conversions.AtoF(positions[2]));
+		try
+		{
+			string[] positions = GameManager.InfoAutreJoueur[$"co{Id}"].Split(";");
+			Position = new Vector3(Lib.Conversions.AtoF(positions[0]), Lib.Conversions.AtoF(positions[1]), Lib.Conversions.AtoF(positions[2]));
+		}
+		catch{}
 	}
 	
 	protected void SetRotation()
--- Jeu/Client/Scripts/PlayerScripts/OtherKnightScript.cs
+++ Jeu/Client/Scripts/PlayerScripts/OtherKnightScript.cs
@@ -3,52 +3,52 @@ using System;
 
 public partial class OtherKnightScript : OtherClassScript
 {
-    public override void _Ready()
-    {
-        InitOtherPlayer();
-        AnimationOtherPlayer = GetNode<AnimationPlayer>("AnimationOtherPlayer");
-    }
-    
-    public override void _Process(double delta)
-    {
-        PseudoManager();
-    }
-    
-    public override void _PhysicsProcess(double delta)
-    {
-        SetPosition();
-        SetRotation();
+	public override void _Ready()
+	{
+		InitOtherPlayer();
+		AnimationOtherPlayer = GetNode<AnimationPlayer>("AnimationOtherPlayer");
+	}
+	
+	public override void _Process(double delta)
+	{
+		PseudoManager();
+	}
+	
+	public override void _PhysicsProcess(double delta)
+	{
+		SetPosition();
+		SetRotation();
 
-        switch (GameManager.InfoAutreJoueur[$"attack{Id}"])
-        {
-            case "hit":
-                AnimationOtherPlayer.Play("Hit");
-                GameManager.InfoAutreJoueur[$"attack{Id}"] = "";
-                break;
-            case "protection":
-                AnimationOtherPlayer.Play("Protection");
-                GameManager.InfoAutreJoueur[$"attack{Id}"] = "";
-                break;
-            case "init":
-                AnimationOtherPlayer.Play("Init");
-                GameManager.InfoAutreJoueur[$"attack{Id}"] = "";
-                break;
-            case "walk":
-                AnimationOtherPlayer.Play("Walk");
-                GameManager.InfoAutreJoueur[$"attack{Id}"] = "";
-                break;
-            case "walkside":
-                AnimationOtherPlayer.Play("WalkSide");
-                GameManager.InfoAutreJoueur[$"attack{Id}"] = "";
-                break;
-            case "walkdiagleft":
-                AnimationOtherPlayer.Play("WalkDiagLeft");
-                GameManager.InfoAutreJoueur[$"attack{Id}"] = "";
-                break;
-            case "walkdiagright":
-                AnimationOtherPlayer.Play("WalkDiagRight");
-                GameManager.InfoAutreJoueur[$"attack{Id}"] = "";
-                break;
-        }
-    }
+		switch (GameManager.InfoAutreJoueur[$"attack{Id}"])
+		{
+			case "hit":
+				AnimationOtherPlayer.Play("Hit");
+				GameManager.InfoAutreJoueur[$"attack{Id}"] = "";
+				break;
+			case "protection":
+				AnimationOtherPlayer.Play("Protection");
+				GameManager.InfoAutreJoueur[$"attack{Id}"] = "";
+				break;
+			case "init":
+				AnimationOtherPlayer.Play("Init");
+				GameManager.InfoAutreJoueur[$"attack{Id}"] = "";
+				break;
+			case "walk":
+				AnimationOtherPlayer.Play("Walk");
+				GameManager.InfoAutreJoueur[$"attack{Id}"] = "";
+				break;
+			case "walkside":
+				AnimationOtherPlayer.Play("WalkSide");
+				GameManager.InfoAutreJoueur[$"attack{Id}"] = "";
+				break;
+			case "walkdiagleft":
+				AnimationOtherPlayer.Play("WalkDiagLeft");
+				GameManager.InfoAutreJoueur[$"attack{Id}"] = "";
+				break;
+			case "walkdiagright":
+				AnimationOtherPlayer.Play("WalkDiagRight");
+				GameManager.InfoAutreJoueur[$"attack{Id}"] = "";
+				break;
+		}
+	}
 }
