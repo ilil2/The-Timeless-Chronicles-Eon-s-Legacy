74c10c2cee4f890273ad3a479b548a567a8577fd
Ilyann Gwinner <ilyann.gwinner@gmail.com>
2024-03-07 11:25:50 +0100

    boss qui marche mieux

--- Jeu/Client/Scripts/EntityScripts/Boss1.cs
+++ Jeu/Client/Scripts/EntityScripts/Boss1.cs
@@ -1,13 +1,14 @@
 using Godot;
 using System;
-public partial class Boss_niv1_Script : CharacterBody3D
+//Ceci est un commentaire
+public partial class IABoss : CharacterBody3D
 {
 	//stats
-	private int speed = 5; // vitesse
-	private int accel = 15; // acceleration
-	private int DistVue = 300; // Distance de vue
-	private int HP = 1000; // health point
-	private bool Alive = true;
+	private int speed = 2; // vitesse
+	private int accel = 10; // acceleration
+	private int DistVue = 30; // Distance de vue
+	private int HP = 100; // health point
+	private int AgroMax = 300; // Valeur Maximum de l'Agro
 	private float tim = -0.1f;
 	
 	//debug
@@ -15,84 +16,47 @@ public partial class Boss_niv1_Script : CharacterBody3D
 	
 	// Pour le pathfiding... ?
 	private NavigationAgent3D Nav; // Cible du pathfiding
-	private RayCast3D Ray; // Activer le pathfiding
+	public int Agro = 0; // stop quand agro = 0
 	private bool SkipFrame = true; // RayCast3D ne fonctionne pas à la première frame
-	private bool IsTooFar = false; // Indique si le mob est trop loin du joueur
 	
 	// Autre
 	private Node Parent;
 	private CharacterBody3D Player = new CharacterBody3D();
-	private Vector3 Me = new Vector3();
-	private Vector3 PlayerPos = new Vector3();
 	private bool PlayerSet = false;
-	protected int RenderDist = 150;
 	private float StateDeath = -0.1f;
-	private Random R = new Ramdom(); 
-	
-	 // Zones d'attaque
-	private Area3D slashArea;
-	private Area3D bumpArea;
-	private Area3D boubouleArea;
 	
 	public override void _Ready()
 	{
 		Parent = GetParent().GetParent();
 		Nav = GetNode<NavigationAgent3D>("NavigationAgent3D");
-		// Création d'un timer pour le cooldown des attacks 
-		attackTimer = new Timer(); 
-		AddChild(attackTimer);
-		attackTimer.WaitTime = 2f; // Cooldown de 2 secondes entre les attaques
-		attackTimer.OneShot = true;
-		attackTimer.Connect("timeout", this, nameof(OnAttackTimerTimeout));
-		
-	   // Initialisation des zones d'attaque
-		slashArea = GetNode<Area3D>("SlashArea");
-		bumpArea = GetNode<Area3D>("BumpArea");
-		boubouleArea = GetNode<Area3D>("BoubouleArea");
-
-		// Connexion des signaux pour les collisions
-		slashArea.Connect("body_entered", this, nameof(OnAttackBodyEntered), new Godot.Collections.Array { "Slash" });
-		bumpArea.Connect("body_entered", this, nameof(OnAttackBodyEntered), new Godot.Collections.Array { "Bump" });
-		boubouleArea.Connect("body_entered", this, nameof(OnAttackBodyEntered), new Godot.Collections.Array { "Bouboule" });
 	}
-	
-	   private void OnAttackBodyEntered(Node body, string attackType)
-	{
-		if (body is Player && Alive)
-		{
-			// Endroit ou il faut mettre chaque chose que fait chaque atk 
-			GD.Print($"{attackType} hit the player!");
-			Player.TakeDamage(CalculateDamage(attackType)); //  CalculateDamage a implenter pour les dégats
-		}
-	}
-
 	
 	public override void _Process(double delta) //NavMesh
 	{
 		if(PlayerSet)
 		{
-			
-
-					var dir = new Vector3();  //Pathfiding
-					var NextPos = Nav.GetNextPathPosition();
-					dir = NextPos - GlobalPosition;
-					dir = dir.Normalized();
-					Velocity = Velocity.Lerp(dir*speed,(float)(accel*delta));
-					MoveAndSlide();
+			if(Distance(Player.Position,this.Position)<=DistVue)
+			{
+				if (Agro > 0) Agro -= 1;
+				var dir = new Vector3();  //Pathfiding
+				var NextPos = Nav.GetNextPathPosition();
+				dir = NextPos - GlobalPosition;
+				dir = dir.Normalized();
+				Velocity = Velocity.Lerp(dir*speed,(float)(accel*delta));
+				MoveAndSlide();
 					
-					try
-					{
-						LookAt(new Vector3(NextPos.X, 1, NextPos.Z)); //Orientation
-					}
-					catch
-					{
-						GD.Print("Still Error");
-					}
-					Rotation = new Vector3(0,Rotation.Y+(float)Math.PI,0);  
+				try
+				{
+					LookAt(new Vector3(NextPos.X, 1, NextPos.Z)); //Orientation
 				}
+				catch
+				{
+					GD.Print("Still Error");
+				}
+				Rotation = new Vector3(0,Rotation.Y+(float)Math.PI,0); 
 			}
 		}
-		if(!PlayerSet && GameManager.Joueur1!=null && Parent.IsAncestorOf(GameManager.Joueur1))
+		if(!PlayerSet && Parent.IsAncestorOf(GameManager.Joueur1))
 		{
 			Player = GameManager.Joueur1;
 			PlayerSet = true;
@@ -110,171 +74,4 @@ public partial class Boss_niv1_Script : CharacterBody3D
 		return Math.Sqrt(Math.Pow(Room1.X - Room2.X, 2) +
 						 Math.Pow(Room1.Z - Room2.Z, 2));
 	}
-	private void SetUpDeath()
-	{
-		GD.Print("Set");
-		ShaderMaterial SH = new ShaderMaterial();
-		SH.Shader = (Shader)ResourceLoader.Load("res://Ressources/Map/death.tres");
-		MeshInstance3D M = GetNode<MeshInstance3D>("Body/Body"); 
-		StandardMaterial3D Mat = (StandardMaterial3D)M.GetActiveMaterial(0);
-		SH.SetShaderParameter("Albedo",Mat.AlbedoTexture);
-		SH.SetShaderParameter("Timer", 0f);
-		M.MaterialOverride = SH;
-		Alive = false;
-		
-	}
-
-	// Processus d'attaque
-	
-	private void OnAttackTimerTimeout() // check le cooldown 
-	{
-		canAttack = true; // Permet au boss d'attaquer à nouveau
-	}
-	
-	private void Attack()
-	{
-		int a;
-		
-		if (HP <= 500) 
-		{
-			state = 3; 
-		}
-		if (state == 2 && Alive)
-		{
-				
-			// Implémentez la logique d'attaque ici
-			
-			a = R.Next(1,3); // choisir atk
-			if (a = 1 )
-			{
-				Slash(); // Exec l'attaque
-				canAttack = false ; // empèche atk overflow
-				attackTimer.Start(); // init cooldown 
-			}
-			
-			if (a = 2 )
-			{
-				Slash(); // Exec l'attaque
-				canAttack = false ; // empèche atk overflow
-				attackTimer.Start(); // init cooldown 
-			}
-			
-		}
-		else if (state == 3)
-		{
-			// Implémentez la logique d'attaque spéciale ici
-			
-			a = R.Next(1,5); // choisir atk & spé 
-			if (a = 1 )
-			{
-				Slash(); // Exec l'attaque
-				canAttack = false ; // empèche atk overflow
-				attackTimer.Start(); // init cooldown 
-			}
-			if (a = 2 )
-			{
-				Bump(); // Exec l'attaque
-				canAttack = false ; // empèche atk overflow
-				attackTimer.Start(); // init cooldown 
-			}
-			if (a = 3 )
-			{
-				Bouboule(); // Exec l'attaque
-				canAttack = false ; // empèche atk overflow
-				attackTimer.Start(); // init cooldown 
-			}
-			if (a = 4)
-			{
-				Buff(); // Exec l'attaque
-				canAttack = false ; // empèche atk overflow
-				attackTimer.Start(); // init cooldown 
-			}
-		}
-	}
-	
-	// Types d'attaques 
-	
-	private void Slash()
-	{
-		CalculateDamage("Slash");
-		ToggleAttackArea(slashArea, true);
-		GD.Print("Je me slash");
-	}
-	private void Bump()
-	{
-		CalculateDamage("Bump");
-		ToggleAttackArea(bumpArea, true);
-		GD.Print("Je te Bump");
-	}
-	private void Buff()
-	{
-		GD.Print("POWAAAAAAAA");
-	}
-	private void Bouboule()
-	{
-		CalculateDamage("Bouboule");
-		ToggleAttackArea(boubouleArea, true);
-		GD.Print("Bouffe mes boules");
-	}
-	
-	private int CalculateDamage(string attackType)
-	{
-		// Mettre les dégats subit par le joueur 
-		switch (attackType)
-		{
-			case "Slash":
-				return 50;
-			case "Bump":
-				return 30;
-			case "Bouboule":
-				return 400;
-			default:
-				return 0; // Pas de dégâts pour Buff ou types inconnus
-		}
-	}
-	
-	private void DisableAttackArea(Area3D area)
-{
-	area.Monitoring = false; // Désactive la zone d'attaque
-}
-
- 
-// Fonction activer/désactiver les zones d'attaque 
-private void ToggleAttackArea(Area3D area, bool isActive)
-{
-	area.Monitoring = isActive; // Active ou désactive la détection de collision
-	if (isActive)
-	{
-		Timer timer = new Timer(); // Création d'un nouveau Timer
-		AddChild(timer); 
-		timer.WaitTime = 0.5f; // Définition du délai avant désactivation de l'area 3D
-		timer.OneShot = true; // Le Timer s'arrête après son exécution
-		// Définition de l'action à effectuer lorsque le Timer atteint 0
-		timer.Connect("timeout", this, "TimeoutHandler", new Godot.Collections.Array { timer, area });
-		timer.Start(); // Démarrage du Timer
-	}
-}
-
-// Timer atteint 0
-private void TimeoutHandler(Godot.Object[] args)
-{
-	if (args.Length == 2 && args[0] is Timer timer && args[1] is Area3D area)
-	{
-		DisableAttackArea(area); // Désactivation de l'aire d'attaque
-		timer.QueueFree(); // Suppression du Timer
-	}
-}
-
-
-	
-	// Dégâts reçus par le boss
-	public void TakeDamage(int damage)
-	{
-		HP -= damage;
-		if (HP <= 0)
-		{
-			SetUpDeath();
-		}
-	}
-	
 }
