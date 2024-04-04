33adb075406ec8ce43a303ebbe516c5fd41dcf24
Corentin del Pozo <darkrentin@gmail.com>
2024-04-04 17:30:44 +0200

    Ajout attaque du MobmongUs

--- Jeu/Client/Scripts/EntityScripts/MobScript.cs
+++ Jeu/Client/Scripts/EntityScripts/MobScript.cs
@@ -1,16 +1,17 @@
 using Godot;
 using System;
 //Ceci est un commentaire
-public partial class MobScript : CharacterBody3D
+public abstract partial class MobScript : CharacterBody3D
 {
 	//stats
-	private int speed = 2; // vitesse
-	private int accel = 10; // acceleration
-	private int DistVue = 30; // Distance de vue
-	private int HP = 100; // health point
-	private int AgroMax = 300; // Valeur Maximum de l'Agro
-	private bool Alive = true;
-	private float tim = -0.1f;
+	protected int speed = 2; // vitesse
+	protected int accel = 10; // acceleration
+	protected int DistVue = 30; // Distance de vue
+	public int HP = 100; // health point
+	protected int AgroMax = 300; // Valeur Maximum de l'Agro
+	public bool Alive = true;
+	private float TimerDeath = -0.1f;
+	protected AnimationPlayer Ani;
 	
 	//debug
 	public bool DebugMode = true; // variable debug mode
@@ -19,7 +20,7 @@ public partial class MobScript : CharacterBody3D
 	private NavigationAgent3D Nav; // Cible du pathfiding
 	private RayCast3D Ray; // Activer le pathfiding
 	public int Agro = 0; // stop quand agro = 0
-	private int state; // -1 = repos | 0 = retour à position initiale | 1 = suivre joueur
+	public int state; // -1 = repos | 0 = retour à position initiale | 1 = suivre joueur | 2 = attaque
 	private Vector3 PosInnit; // position initiale
 	private Vector3 RotInnit; // rotation innitiale
 	private bool SkipFrame = true; // RayCast3D ne fonctionne pas à la première frame
@@ -34,7 +35,7 @@ public partial class MobScript : CharacterBody3D
 	protected int RenderDist = 150;
 	private float StateDeath = -0.1f;
 	
-	public override void _Ready()
+	public void Ready()
 	{
 		Parent = GetParent().GetParent();
 		PosInnit = Position;
@@ -42,19 +43,24 @@ public partial class MobScript : CharacterBody3D
 		Nav = GetNode<NavigationAgent3D>("NavigationAgent3D");
 		Nav.TargetPosition = PosInnit;
 		Ray = GetNode<RayCast3D>("Ray");
+		Ani = GetNode<AnimationPlayer>("Animation");
 		state = -1;
 		
 	}
 
-	public override void _PhysicsProcess(double delta) //Raycast
+	public void PhysicsProcess(double delta) //Raycast
 	{
 		if(PlayerSet)
 		{
 			if(Distance(Player.Position,this.Position)<=DistVue)
 			{
-				if (Distance(Player.Position,this.Position)<=1 && Alive)
+				if (Distance(Player.Position,this.Position)<=2 && Alive)
 				{
-					SetUpDeath();
+					state = 2;
+				}
+				else if(state == 2)
+				{
+					state = 1;
 				}
 				if(!Alive)
 				{
@@ -65,8 +71,6 @@ public partial class MobScript : CharacterBody3D
 					IsTooFar = !(Distance(Player.Position,this.Position)<=DistVue);
 					Ray.Rotation = new Vector3(0,-Rotation.Y,0);
 					Ray.TargetPosition = new Vector3(Player.GlobalPosition.X - GlobalPosition.X, 1 , Player.GlobalPosition.Z - GlobalPosition.Z );
-					Label3D St = GetNode<Label3D>("State");
-					St.Text = $"State: {state}";
 					if (state == 0 || state == -1) // Si ne suit pas le joueur
 					{
 						if (!Ray.IsColliding() && !IsTooFar) // si rien entre mob et joueur
@@ -92,7 +96,7 @@ public partial class MobScript : CharacterBody3D
 			}
 		}
 	}
-	public override void _Process(double delta) //NavMesh
+	public void Process(double delta) //NavMesh
 	{
 		if(PlayerSet && Alive)
 		{
@@ -138,6 +142,16 @@ public partial class MobScript : CharacterBody3D
 		}
 	}
 	
+	public virtual void TakeDamage(int damage)
+	{
+		HP -= damage;
+		if(HP<=0)
+		{
+			Ani.Stop();
+			SetUpDeath();
+		}
+	}
+	
 	public void SetDebug()
 	{
 		DebugMode = !DebugMode;
@@ -148,9 +162,9 @@ public partial class MobScript : CharacterBody3D
 		return Math.Sqrt(Math.Pow(Room1.X - Room2.X, 2) +
 						 Math.Pow(Room1.Z - Room2.Z, 2));
 	}
-	private void SetUpDeath()
+	protected void SetUpDeath()
 	{
-		GD.Print("Set");
+		Alive = false;
 		ShaderMaterial SH = new ShaderMaterial();
 		SH.Shader = (Shader)ResourceLoader.Load("res://Ressources/Map/death.tres");
 		MeshInstance3D M = GetNode<MeshInstance3D>("Body/Body"); 
@@ -158,15 +172,14 @@ public partial class MobScript : CharacterBody3D
 		SH.SetShaderParameter("Albedo",Mat.AlbedoTexture);
 		SH.SetShaderParameter("Timer", 0f);
 		M.MaterialOverride = SH;
-		Alive = false;
 		
 	}
 	private void Death()
 	{
-		tim += 0.01f;
+		TimerDeath += 0.01f;
 		MeshInstance3D M = GetNode<MeshInstance3D>("Body/Body"); 
-		(M.MaterialOverride as ShaderMaterial).SetShaderParameter("Timer",tim);
-		if(tim>=1.1f)
+		(M.MaterialOverride as ShaderMaterial).SetShaderParameter("Timer",TimerDeath);
+		if(TimerDeath>=1.1f)
 		{
 			QueueFree();
 		}
--- Jeu/Client/Scripts/EntityScripts/Mobmongus.cs
+++ Jeu/Client/Scripts/EntityScripts/Mobmongus.cs
@@ -3,9 +3,14 @@ using System;
 
 public partial class Mobmongus : MobScript
 {
+	private Label3D St;
+	private Label3D Hp;
 	public override void _Ready()
 	{
 		Ready();
+		St = GetNode<Label3D>("State");
+		Hp = GetNode<Label3D>("HP");
+		Hp.Text = $"HP: {HP}";
 	}
 	public override void _PhysicsProcess(double delta)
 	{
@@ -14,5 +19,25 @@ public partial class Mobmongus : MobScript
 	public override void _Process(double delta) 
 	{
 		Process(delta);
+		//St.Text = $"State: {state}";
+		if(state == 2)
+		{
+			if(Ani.CurrentAnimation != "Hit" && Alive)
+			{
+				Ani.Play("Hit");
+				TakeDamage(10);
+			}
+		}
+		
+	}
+	public override void TakeDamage(int damage)
+	{
+		HP -= damage;
+		Hp.Text = $"HP: {HP}";
+		if(HP<=0)
+		{
+			Ani.Stop();
+			SetUpDeath();
+		}
 	}
 }
