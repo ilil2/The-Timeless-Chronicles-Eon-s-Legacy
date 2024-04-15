b19138e36787078728ca0b9aaf510f20fc27f49c
Ilyann Gwinner <ilyann.gwinner@gmail.com>
2024-04-15 13:06:16 +0200

    open & close portal sound

--- Jeu/Client/Scripts/MapScript/Object/Portal.cs
+++ Jeu/Client/Scripts/MapScript/Object/Portal.cs
@@ -5,12 +5,18 @@ public partial class Portal : Node3D
 {
 	private AnimationPlayer _animationPortal;
 	private AudioStreamPlayer3D _idleSound;
+	private AudioStreamPlayer3D _openSound;
+	private AudioStreamPlayer3D _closeSound;
+
+	private bool _isOpen = false;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		_animationPortal = GetNode<AnimationPlayer>("AnimationPlayer");
 		_idleSound = GetNode<AudioStreamPlayer3D>("Idle");
+		_openSound = GetNode<AudioStreamPlayer3D>("Open");
+		_closeSound = GetNode<AudioStreamPlayer3D>("Close");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,13 +25,17 @@ public partial class Portal : Node3D
 		if (_animationPortal.CurrentAnimation == "Idle" && _idleSound.Playing == false)
 		{
 			_idleSound.Play();
-			GD.Print("Play Idle Sound");
 		}
-
-		if (!_animationPortal.IsPlaying())
+		else if (_animationPortal.CurrentAnimation == "Open" && !_isOpen)
+		{
+			_openSound.Play();
+			_isOpen = true;
+		}
+		if (_animationPortal.CurrentAnimation == "Close" && _isOpen)
 		{
 			_idleSound.Stop();
-			GD.Print("Stop Idle Sound");
+			_closeSound.Play();
+			_isOpen = false;
 		}
 	}
 }
