87b130191b1d0be66343f18c8d03532e4ecda32f
Corentin del Pozo <darkrentin@gmail.com>
2024-04-30 13:15:29 +0200

    Fix FogSkeleton

--- Jeu/Client/Scripts/EntityScripts/Mob/FogSkeleton.cs
+++ Jeu/Client/Scripts/EntityScripts/Mob/FogSkeleton.cs
@@ -5,14 +5,20 @@ public partial class FogSkeleton : MobScript
 {
 	private AnimationTree AniTree;
 	private bool Fog;
-	private bool IsHide = true;
+	[Export] private bool IsHide = true;
+	private AnimationNodeStateMachinePlayback PlayBack;
 	public override void _Ready()
 	{
 		Ready();
 		AniTree = GetNode<AnimationTree>("AnimationTree");
+		
 	}
 	public override void _PhysicsProcess(double delta)
 	{
+		if (PlayBack == null)
+		{
+			PlayBack = (AnimationNodeStateMachinePlayback)AniTree.Get("parameters/playback");
+		}
 		PhysicsProcess(delta);
 		Fog = (GetParent() as TestFog).Fog;
 		CanDo = !IsHide;
@@ -20,30 +26,28 @@ public partial class FogSkeleton : MobScript
 	public override void _Process(double delta) 
 	{
 		Process(delta);
+		GD.Print(IsHide);
 		if(!Fog && !IsHide)
 		{
-			AniTree.Set("parameters/conditions/Hide",true);
-			AniTree.Set("parameters/conditions/Show",false);
+			PlayBack.Travel("StandDown");
 			IsHide = true;
 		}
 		else if(Fog && IsHide)
 		{
-			AniTree.Set("parameters/conditions/Hide",false);
-			AniTree.Set("parameters/conditions/Show",true);
-			IsHide = false;
+			PlayBack.Travel("StandUp");
 		}
 		if(!IsHide&&Alive&&(state == 1 || state == 3 || state == 0))
 		{
 			if(Ani.CurrentAnimation != "Hit" && Ani.CurrentAnimation!="Walk")
 			{
-				Ani.Play("Walk");
+				PlayBack.Travel("Walk");
 			}
 		}
 		if(!IsHide&&state == 2)
 		{
 			if(Ani.CurrentAnimation != "Atk" && Ani.CurrentAnimation != "Hit" && Alive)
 			{
-				Ani.Play("Atk");
+				PlayBack.Travel("Atk");
 			}
 		}
 		if(!IsHide&&state == -1)
@@ -63,7 +67,7 @@ public partial class FogSkeleton : MobScript
 				GD.Print("Mort");
 				Alive = false;
 				Ani.Stop();
-				Ani.Play("Death");
+				PlayBack.Travel("Death");
 			}
 			else
 			{
@@ -72,3 +76,4 @@ public partial class FogSkeleton : MobScript
 		}
 	}
 }
+
