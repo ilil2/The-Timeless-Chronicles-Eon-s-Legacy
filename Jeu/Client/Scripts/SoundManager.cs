5137988cf0be349b28dce46908311073aa44282a
Ilyann Gwinner <ilyann.gwinner@gmail.com>
2024-06-11 14:26:13 +0200

    ajout toutes les musiques

--- Jeu/Client/Scripts/SoundManager.cs
+++ Jeu/Client/Scripts/SoundManager.cs
@@ -4,11 +4,17 @@ using System;
 public partial class SoundManager : Node
 {
 	private AudioStreamPlayer _shop;
+	private AudioStreamPlayer _menu;
+	private AudioStreamPlayer _map1;
+	private AudioStreamPlayer _map2;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		_shop = GetNode<AudioStreamPlayer>("Shop");
+		_menu = GetNode<AudioStreamPlayer>("Menu");
+		_map1 = GetNode<AudioStreamPlayer>("Lvl1");
+		_map2 = GetNode<AudioStreamPlayer>("Lvl2");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,11 +28,36 @@ public partial class SoundManager : Node
 				{
 					_shop.Play();
 				}
+
+				else if (GameManager.Map is MapLvl1Script && _map1.Playing == false)
+				{
+					_map1.Play();
+				}
+				
+				else if (GameManager.Map is MapLvl2Script && _map2.Playing == false)
+				{
+					_map2.Play();
+				}
+
+				else
+				{
+					_shop.Stop();
+					_map1.Stop();
+					_map2.Stop();
+					_menu.Stop();
+				}
 			}
 		}
 		else
 		{
 			_shop.Stop();
+			_map1.Stop();
+			_map2.Stop();
+
+			if (_menu.Playing == false)
+			{
+				_menu.Play();
+			}
 		}
 	}
 }
