38157165291b67ca9a369f69e8c25c9f3d657542
Otto303 <ottodebrie@gmail.com>
2024-06-11 15:05:52 +0200

    Dialogue corrigés

--- Jeu/Client/Scenes/MapScenes/Shop/ICorentinException.cs
+++ Jeu/Client/Scenes/MapScenes/Shop/ICorentinException.cs
@@ -1,15 +1,14 @@
-using Godot;
 using System;
 
-public partial class ICorentinException : Node
+public partial class ICorentinException : Exception
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	public ICorentinException()
 	{
 	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public ICorentinException(string message) : base(message)
+	{
+	}
+	public ICorentinException(string message, Exception inner) : base(message, inner)
 	{
 	}
 }
