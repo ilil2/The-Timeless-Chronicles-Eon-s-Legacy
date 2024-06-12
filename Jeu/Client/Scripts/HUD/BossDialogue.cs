a3ff772e3464508638c43236babe705b5f7dd420
Otto303 <ottodebrie@gmail.com>
2024-06-12 11:16:19 +0200

    BossDialogue.tsn (compile, mais ne marche probablement pas...)

--- Jeu/Client/Scripts/HUD/BossDialogue.cs
+++ Jeu/Client/Scripts/HUD/BossDialogue.cs
@@ -9,7 +9,7 @@ public partial class BossDialogue : Area3D
 {
 	public bool Near = false;
 	private IMap Parent;
-	private Camera3D Cam;
+	//private Camera3D Cam;
 	private Control Hud;
 	private Label Line;
 	private Dictionary<string,Dictionary<string,string>> Dialogue;
@@ -20,7 +20,7 @@ public partial class BossDialogue : Area3D
 	public override void _Ready()
 	{
 		Parent = GetParent<IMap>();
-		Cam = GetNode<Camera3D>("Cam");
+		//Cam = GetNode<Camera3D>("Cam");
 		Hud = GetNode<Control>("DialogueHud");
 		Line = Hud.GetNode<Label>("Line");
 		Dialogue = JsonSerializer.Deserialize<Dictionary<string,Dictionary<string,string>>>(File.ReadAllText("Ressources/Dialogue/BossDialogue.json"));
@@ -41,25 +41,20 @@ public partial class BossDialogue : Area3D
 		}
 	}
 	
-	private void _on_close_button_pressed()
-	{
-		Hud.Visible = false;
-		Near = false;
-		Cam.Current = false;
-		Parent.CamOnPlayer = true;
-		Hud.Visible = false;
-		GameHUD.IsVisible = false;
-		Parent.GetParent().GetNode<Control>("GameHUD").Visible = true;
-		Input.MouseMode = Input.MouseModeEnum.Captured;	
-	}
-	
 	private string GetDialogue()
 	{
 		(int c1, int c2) = SaveDialogue.Boss;
 		c2++;
 		if(Dialogue[c1.ToString()][c2.ToString()]=="END")
-		{
-			_on_close_button_pressed();
+		{	
+			Hud.Visible = false;
+			Near = false;
+			//Cam.Current = false;
+			Parent.CamOnPlayer = true;
+			Hud.Visible = false;
+			GameHUD.IsVisible = false;
+			Parent.GetParent().GetNode<Control>("GameHUD").Visible = true;
+			Input.MouseMode = Input.MouseModeEnum.Captured;	
 		}
 		SaveDialogue.Boss = (c1, c2);
 		GD.Print($"{c1} {c2}");
