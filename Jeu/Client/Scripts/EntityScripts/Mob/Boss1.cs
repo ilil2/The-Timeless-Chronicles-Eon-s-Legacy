c467d331a8cfaf0048a359aa5072fce3e521f863
Otto303 <ottodebrie@gmail.com>
2024-06-12 11:25:21 +0200

    Boss Lv1 (avec 1 seule atk)

--- Jeu/Client/Scripts/EntityScripts/Mob/Boss1.cs
+++ Jeu/Client/Scripts/EntityScripts/Mob/Boss1.cs
@@ -1,46 +1,60 @@
 using Godot;
 using System;
+using JeuClient.Scripts.EntityScripts.Mob;
+using JeuClient.Scripts.PlayerScripts;
 
-public partial class Boss1 : MobScript
+public partial class Boss1 : Boss
 {
-	private IMap map;
+	private PackedScene Son = GD.Load<PackedScene>("res://Scenes/EntityScenes/Mob/Boss2.tscn");
+	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		Ready();
-		speed = 3;
-		HP = 20;
-		map = (IMap)GetParent();
+		DistAtk = (int)(DistAtk*Scale.X);
+
 	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		Process(delta);
+	}
+
 	public override void _PhysicsProcess(double delta)
 	{
 		PhysicsProcess(delta);
-		if(HP<=0)
+	}
+
+	public override void TakeDamage(int damage, int id, bool send = true)
+	{
+		Agro[id] += 10;
+		if(Alive)
 		{
-			map.CanExit = true;
+			HP -= damage;
+			GD.Print("HP" + HP);
+			if(HP<=0)
+			{
+				GD.Print("Mort");
+				Alive = false;
+				Ani.Play("Death");
+				GameManager.Gold += 10;
+				GameManager.xp += 1;
+				GameManager.InfoJoueur[$"ia"] += $"{ID}°{42}°{Position.X}?{Position.Z}°{(GameManager.Joueur1 as ClassScript).Id}=";
+			}
+			else
+			{
+				//Ani.Play("Hit");
+			}
+			if (send)
+			{
+				GameManager.InfoJoueur[$"ia"]  += $"{ID}°TK§{damage}°{Position.X}?{Position.Z}=";
+			}
+			
 		}
 	}
-	public override void _Process(double delta) 
+	
+	public override void AtDeath()
 	{
-		Process(delta);
-		if(Alive && (state == 1 || state == 3 || state == 0))
-		{
-			if(Ani.CurrentAnimation != "Hit" && Ani.CurrentAnimation!="Walk")
-			{
-				Ani.Play("Walk");
-			}
-		}
-		if(state == 2)
-		{
-			if(Ani.CurrentAnimation != "Atk" && Ani.CurrentAnimation != "Hit" && Alive)
-			{
-				Ani.Play("Atk");
-			}
-		}
-		if(state == -1)
-		{
-			Ani.Stop();
-		}
-		
+		//Do nothing
 	}
 }
-
